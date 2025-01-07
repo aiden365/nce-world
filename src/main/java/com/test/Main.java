cefb5b64fae80f7a5e89e709bda3d5950d2c8d18
aidenc <chenjiacheng365@qq.com>
2025-01-08 03:24:07 +0800

    这个人很懒，什么都没写。

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -42,18 +42,7 @@ public class Main {
 
     @SneakyThrows
     public static void main(String[] args) {
-        //能够引导非限定性定语从句的关系代词={as,which,who,whom}，关系副词={when,where}
-        //to do with Thor 译与...有关
-        //call sth1 of sth2 称呼sth1为sth2
-        //Within Midgard lay Asgard 倒装句 强调主语在midgard之内
-        //resorted [vi.] 与to连用表示采用某种手段或方法
-        //refer to A as B 把A称作B
-        //to hold something in check 表示对某事物或某人进行控制或抑制，以防止其过度发展或产生负面影响，例如 the one of the vital ways of taking success was to hold ourselves temper in check
-        //His hammer could do more than make rain 这句话中do是中心谓语动词，make之所以用动词原型，它是不带to的动词不定式，虽然这是一个固定搭配，英语中有一些动词后若跟不定式做宾语补足语，余姚使用不带to的不定式形式，如四看：notice, observe, see, watch 三使役：have, let, make 二听：hear, listen to 一感觉：feel 半帮助：help 例如：I saw him leave the house
-        //struggle against 与...做斗争
-        //sit by [vi.] 坐视不管，等价于sit on one`s hands 例如，we can not [sit by / sit on our hands] and do nothing about these problems
-        //make sth. to sb. 给某人某物，等价于offer sth. to sb.或offer sb. sth.
-        //offer to do sth. 主动去做某事
+
         String aa = "E:\\workspace\\java\\nce-world\\doc\\aa.txt";
         try(FileOutputStream os = new FileOutputStream(new File(aa))){
             os.write("hello".getBytes(StandardCharsets.UTF_8));
@@ -67,15 +56,7 @@ public class Main {
 
     @Test
     public void main2() {
-        // This they did by performing various religious ceremonies, or rites.
-        // The most significant religious ceremony in Norse times was the offering.
-        // Making an offering to a god had the effect of increasing that god’s power.
-        // For example, mortals had to make offerings to the gods to give them the strength to conquer the forces of chaos.
-        // They could do this by sacrificing an animal to the god.
-        // The offering to Thor was usually a goat.
-        // Offerings to Odin sometimes took the form of human sacrifices.
-        // The myth that is best known in the Nordic countries comes from the Eddie poem “The Lay of Thrym.”
-        // It tells how Thor, rising from sleep, finds that his hammer is gone.
+        //Then the god Heimdall
 
         String filePath = System.getProperty("user.dir").concat("/temp/lock.txt");
         try (FileChannel open = FileChannel.open(Path.of(filePath), StandardOpenOption.WRITE)){
