b678044e9e1ffe5a28b229fcccecfd104dba4630
chenjiacheng1 <chenjiacheng365@qq.com>
2025-01-08 18:05:18 +0800

    这个人很懒，什么都没写。

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -42,7 +42,14 @@ public class Main {
 
     @SneakyThrows
     public static void main(String[] args) {
-
+        //have control over sth. 控制，支配。其中control是名词，作名词是通常加介词，如over + 介宾 表示控制的对象
+        //dress up [vi.] 打扮的漂漂亮亮的，可接for、as引导的状语，如she likes to dress up for a party
+        //ever [adv.] 在否定句中加强否定表示永远不，在肯定剧中加强肯定表示确实...
+        //attire in [vt.] 穿上...，类似的还有 put on，wear，
+        //in [adj. / n.] terms 译为从...角度来看，例如in the long terms, in relative terms，in the present terms，也可以写成in terms of + [介词宾语] 例如in terms of how people feel
+        //disguised as sth. [np.] 伪装成...，也可以用动词形式 V. disguise A as A1
+        //breach [vt.] 突破，
+        //
         String aa = "E:\\workspace\\java\\nce-world\\doc\\aa.txt";
         try(FileOutputStream os = new FileOutputStream(new File(aa))){
             os.write("hello".getBytes(StandardCharsets.UTF_8));
@@ -56,8 +63,9 @@ public class Main {
 
     @Test
     public void main2() {
-        //Then the god Heimdall
-
+        // He also drinks three barrels of beer.
+        // This astonishes Thrym. The true identity of the “commandos” is very nearly revealed.
+        // But Loki manages to avert the danger by explaining that Freyja has been looking forward to coming to jotunheim so much that she has not eaten for a week. When Thrym lifts the bridal veil to kiss the bride, he is startled to find himself looking into Thor’s burning eyes.
         String filePath = System.getProperty("user.dir").concat("/temp/lock.txt");
         try (FileChannel open = FileChannel.open(Path.of(filePath), StandardOpenOption.WRITE)){
             FileLock fileLock = open.lock();
