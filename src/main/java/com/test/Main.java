1c25120ba5915152b963d6e6ed4d6fed2a36a0c2
chenjiacheng1 <chenjiacheng365@qq.com>
2025-01-10 18:05:42 +0800

    这个人很懒，什么都没写。

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -17,6 +17,8 @@ import java.nio.channels.FileLock;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.List;
 
 import static com.words.TestFour4.*;
 import static com.words.TestCz.*;
@@ -42,14 +44,26 @@ public class Main {
 
     @SneakyThrows
     public static void main(String[] args) {
-        //have control over sth. 控制，支配。其中control是名词，作名词是通常加介词，如over + 介宾 表示控制的对象
-        //dress up [vi.] 打扮的漂漂亮亮的，可接for、as引导的状语，如she likes to dress up for a party
-        //ever [adv.] 在否定句中加强否定表示永远不，在肯定剧中加强肯定表示确实...
-        //attire in [vt.] 穿上...，类似的还有 put on，wear，
-        //in [adj. / n.] terms 译为从...角度来看，例如in the long terms, in relative terms，in the present terms，也可以写成in terms of + [介词宾语] 例如in terms of how people feel
-        //disguised as sth. [np.] 伪装成...，也可以用动词形式 V. disguise A as A1
-        //breach [vt.] 突破，
-        //
+        //So far 迄今为止
+        //take a glimpse 使用take + 具有动作意义的名词 可以表示做这个动作
+        //Mythological notions of this kind 译为这种类型的宗教观念，他和this kind of Mythological notions 这种神话观念
+        //tamper [vi.] 与with连用表示篡改
+        //now that 从属连词，译为既然，由于，可引导让步或原因状语从句
+        //in written form 方式状语，以书面形式
+        //nothing but 等价于only译为只不过，类似的用法还有anything but 等价于not at all或never译为根本不；all but 等价于almost译为几乎；none but等价于no one except 译为只有
+        //from about 570 B.C. from [prep.]表示时间的起点 about [prep.] 译为大约
+        //recourse to sth. 求助，依赖
+        //develop from a to b 从a到b的发展
+        //rather than [conj.] 而不是
+        //at all [adv.] 完全，根本
+        //work out [vt.] 得出
+        //but then [conj.] 表转折，常译为然而
+        //and that [conj.] 表递进，常译为而且
+        //come out [vt.] 出现
+        //a few [locks/strands] of hair : 几缕头发
+        //in the end [adv.] 常位于句末，译为最终
+        //in the time before 介词短语，译为在某个时间之前
+        //look over one's shoulder [vp.] 严密监视
         String aa = "E:\\workspace\\java\\nce-world\\doc\\aa.txt";
         try(FileOutputStream os = new FileOutputStream(new File(aa))){
             os.write("hello".getBytes(StandardCharsets.UTF_8));
@@ -63,19 +77,23 @@ public class Main {
 
     @Test
     public void main2() {
-        // He also drinks three barrels of beer.
-        // This astonishes Thrym. The true identity of the “commandos” is very nearly revealed.
-        // But Loki manages to avert the danger by explaining that Freyja has been looking forward to coming to jotunheim so much that she has not eaten for a week. When Thrym lifts the bridal veil to kiss the bride, he is startled to find himself looking into Thor’s burning eyes.
+        // Is there a basic substance that everything else is made of?
+        //     * Can water turn into wine?
+        //     * How can earth and water produce a live frog!
         String filePath = System.getProperty("user.dir").concat("/temp/lock.txt");
         try (FileChannel open = FileChannel.open(Path.of(filePath), StandardOpenOption.WRITE)){
             FileLock fileLock = open.lock();
+            fileLock.release();
+            substance();
+
+
             FileLock fileLock1 = open.tryLock();
             fileLock1.release();
             Channel channel = fileLock1.acquiredBy();
             Sw.sector1();
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
             ByteArrayOutputStream os = new ByteArrayOutputStream();
-            Sw.sector1();
+
 
 
         } catch (IOException e) {
@@ -86,7 +104,9 @@ public class Main {
     @Test
     public void test66() {
 
-        ClassUtil.scanPackage("com.words").stream().map(Class::getName).forEach(e -> System.out.println("import static " + e + ".*;"));
+        //ClassUtil.scanPackage("com.words").stream().map(Class::getName).forEach(e -> System.out.println("import static " + e + ".*;"));
+        List<Integer> integers = Arrays.asList(1, 2, 3, 4);
+        List<Integer> integers1 = Arrays.asList(2, 4, 6, 8);
 
     }
 }
