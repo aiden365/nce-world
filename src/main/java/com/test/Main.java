29a955847086624e7368d639a0d1ee26972899a8
chenjiacheng1 <chenjiacheng365@qq.com>
2025-01-14 20:00:39 +0800

    这个人很懒，什么都没写。

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -53,6 +53,21 @@ public class Main {
         //full of 译为：充满...
         //mean sth1. by sth2. 通过某物(sth2)表达某种意思(sth1)，或者某物(sth2)的意思是某意思(sth1)，例如:what do you mean by happiness? I mean health by happiness
         //In the way that 以...方式
+        //that which 可以将that单纯的理解成代词并做后面which定语从句的先行词，然后which引导定语从句去补充说明that的信息呢，这样有点像一个what做融合性关系代词
+        //anything常用于否定句中
+        //familiar with 熟悉，熟知。注意不要和similar to 搞混，后者表示与...相似
+        //even more [adv.] 更加，进一步
+        //a far cry from  与...有很大差别、有天壤之别
+        //as 指处于某种状态、 性质、情况、工作等之中
+        //either 这里的either只是用来强调而这只能选择一个，也可以写成to choose between either relying on his senses or his reason, he chose reason.
+        //equate a with b 把a和b视为相等
+        //tally with 与...一致
+        //perceptual knowledge 感性认识
+        //a contemporary of sb. 与某人同时代。例如he is a contemporary of Einstein
+        //for the + 序数词 + time 表示第几次
+        //know 表示经历，例如 she had known both happiness and sadness
+        //take [(a) / (no)] pleasure in sth. 从...中得到乐趣，she take a pleasure in reading books，类似的还有she take a living by selling books
+        //full [adj.] 吃饱的
         String aa = "E:\\workspace\\java\\nce-world\\doc\\aa.txt";
         try(FileOutputStream os = new FileOutputStream(new File(aa))){
             os.write("hello".getBytes(StandardCharsets.UTF_8));
@@ -62,24 +77,21 @@ public class Main {
     }
 
 
-
-
     @Test
     public void main2() {
-        // but it seems clear that he was not thinking of a known substance in the way that Thales had envisaged. Perhaps he meant that the substance which is the source of all things had to be something other than the things created. Because all created things are limited, that which comes before and after them must be “boundless.” It is clear that this basic stuff could not be anything as ordinary as water.
+        //“God is day and night, winter and summer, war and peace, hunger and satiety,” he said.
+        // He used the term “God,” but he was clearly not referring to the gods of the mythology. To Heraclitus, God—or the Deity—was something that embraced the whole world. Indeed, God can be seen most clearly in the constant transformations and contrasts of nature.
         String filePath = System.getProperty("user.dir").concat("/temp/lock.txt");
         try (FileChannel open = FileChannel.open(Path.of(filePath), StandardOpenOption.WRITE)){
             FileLock fileLock = open.lock();
             fileLock.release();
-
-            cock();
             FileLock fileLock1 = open.tryLock();
             fileLock1.release();
             Channel channel = fileLock1.acquiredBy();
-            //only the book is mine on the shelf
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
             ByteArrayOutputStream os = new ByteArrayOutputStream();
 
+            opposite();
 
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -88,11 +100,9 @@ public class Main {
 
     @Test
     public void test66() {
-
         //ClassUtil.scanPackage("com.words").stream().map(Class::getName).forEach(e -> System.out.println("import static " + e + ".*;"));
         List<Integer> integers = Arrays.asList(1, 2, 3, 4);
         List<Integer> integers1 = Arrays.asList(2, 4, 6, 8);
-
-
+        System.out.println();
     }
 }
